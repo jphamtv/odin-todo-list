c04252fe3539ffd1b44e9cb7ed3b253ce2550f12
J <jpham26@gmail.com>
2023-10-27 17:34:51 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Todolist App',
+      template: './src/template.html',
+    }),
+  ],
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
